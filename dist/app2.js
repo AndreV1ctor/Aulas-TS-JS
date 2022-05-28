c54a39eecebe9feaf84bb6bb0bc2b0f7b29634b0
André Victor <andre.leonidas458@gmail.com>
2022-05-28 00:57:50 -0300

    Adicionando

---
+++ dist/app2.js
@@ -0,0 +1,31 @@
+"use strict";
+var Carro = /** @class */ (function () {
+    function Carro(modelo, numeroDePortas) {
+        this.velocidade = 0;
+        this.modelo = modelo;
+        this.numeroDePortas = numeroDePortas;
+    }
+    Carro.prototype.acelerar = function () {
+        this.velocidade = this.velocidade + 10;
+    };
+    Carro.prototype.parar = function () {
+        this.velocidade = 0;
+    };
+    Carro.prototype.velocidadeAtual = function () {
+        return this.velocidade;
+    };
+    return Carro;
+}());
+var Concessionaria = /** @class */ (function () {
+    function Concessionaria() {
+    }
+    Concessionaria.prototype.fornecerEndereco = function () {
+        return this.endereco;
+    };
+    Concessionaria.prototype.mostrarListaDeCarros = function () {
+        return this.listaDeCarros;
+    };
+    return Concessionaria;
+}());
+var concessionaria = new Concessionaria('Av Norte');
+console.log(concessionaria);
